c4d91624cb1fe8a35818c9d6d8421b24ead1b997
to232 <tyceolaveson@gmail.com>
2024-03-14 10:47:49 -0600

    Created driver function

---
+++ DerivativeDriver.m
@@ -0,0 +1,13 @@
+function [y,dy] = DerivativeDriver(method,f,x,args)
+% This function will evaluate the derivative of function f at the location
+% x using method.
+
+method = lower(method);
+
+switch method
+    case 'dualdiff'
+        [y,dy] = DualDiff(f,x);
+    otherwise
+        disp("Undefined derivative method")
+end
+
