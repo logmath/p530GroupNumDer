ab498732e8a146e26436ad44b6bbdbaa37c7b01b
Logan Mathews <lmathew3@byu.edu>
2024-03-14 13:32:21 -0600

    First working version of Richardson

--- richExtrapDiff.m
+++ richExtrapDiff.m
@@ -4,28 +4,33 @@
 % Author: Logan Mathews
 %
 
-function dfun = richExtrapDiff(fun,x,h0)
+function [dfun,hfinal] = richExtrapDiff(fun,x,h0)
 ntab = 10;
-cdiff = @(fun,x,h) (fun(x+h) - f(x-h))/(2*h); % centered difference formula
-df(1,1) = cdiff(fun,x,h0);
+cdiff = @(fun,x,h) (fun(x+h) - fun(x-h))/(2*h); % centered difference formula
+df(1,1,:) = cdiff(fun,x,h0);
+fac = 2;
 h(1) = h0;
 err = inf;
 for i = 1:ntab-1
-    fac = 2; % reduce step size by factor of 2
+    %fac = con; % reduce step size by factor of 2
     h(i+1) = h(i)/fac;
-    df(1,i+1) = cdiff(fun,x,h(i+1)); % try smaller step size
+    df(1,i+1,:) = cdiff(fun,x,h(i+1)); % try smaller step size
+    currFac = fac^2;
     for j = 1:i
         % perform Richardson extrapolation to higher order
-        df(j+1,i+1) = (df(j,i+1)*fac - a(j,i)) / (fac^(2*j) - 1);
+        df(j+1,i+1,:) = (df(j,i+1,:)*currFac - df(j,i,:)) / (currFac - 1);
+        currFac = currFac*fac^2;
         % find maximum error of this iteration
-        errij = max(abs(df(j+1,i+1)-df(j,i+1)),abs(df(j+1,i+1)-df(j,i)));
+        errij = max(sum(abs(df(j+1,i+1,:)-df(j,i+1,:))),sum(abs(df(j+1,i+1,:)-df(j,i,:))));
         if errij <= err
             err = errij;
-            dfun = df(j+1,i+1);
+            dfun = squeeze(df(j+1,i+1,:));
+            hfinal = h(end);
         end
     end
-    if abs(df(j+1,i+1)-df(j,i)) >= fac*err
+    if sum(abs(df(j+1,i+1,:)-df(j,i,:))) >= fac*err
         % if error is 2x or more worse than the best error so far, break.
+        disp('hey!')
         break
     end
 end
