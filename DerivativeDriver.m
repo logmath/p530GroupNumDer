765bdea9f2f73b69c2ccd4c359ba6ef65ff097c6
to232 <tyceolaveson@gmail.com>
2024-03-18 11:45:22 -0600

    Framework for part c and updates to the y helper function

--- DerivativeDriver.m
+++ DerivativeDriver.m
@@ -1,14 +1,14 @@
-function [y,dy] = DerivativeDriver(method,f,x,args)
+function [df,y] = DerivativeDriver(method,f,x,args)
 % This function will evaluate the derivative of function f at the location
-% x using method.
+% x using method. args needs to come in as a cell array
 
 method = lower(method);
 
 switch method
-    case 'dualdiff'
-        [y,dy] = DualDiff(f,x);
+    case {'dual', 'automatic'}
+        [y,df] = DualDiff(f,x);
     case {'richextrap','richardson'}
-        [df,y] = richExtrapDiff(f,x);
+        [df,y] = richExtrapDiff(f,x,args{:});
     otherwise
         disp("Undefined derivative method")
 end
